cabdc7b10efe8dc05774562b062ec07fdd8e6c75
kzinthant <kzinthant.d3v@gmail.com>
2020-10-12 16:44:59 +0630

    deployment

---
+++ precache-manifest.14b7fa85839af1287a4c06a2bb7b60be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70732e80c6e0a32a5099f5456cc9509f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14ebbc748991a18b3a3f",
+    "url": "/static/css/main.3c66b28b.chunk.css"
+  },
+  {
+    "revision": "6edcad16919c55819fbe",
+    "url": "/static/js/2.1f752b1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f752b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14ebbc748991a18b3a3f",
+    "url": "/static/js/main.67225427.chunk.js"
+  },
+  {
+    "revision": "7c52fe0ee07bd51e7921",
+    "url": "/static/js/runtime-main.8cfe03bf.js"
+  }
+]);
